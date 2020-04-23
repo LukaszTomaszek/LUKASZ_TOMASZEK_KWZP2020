1f8f07197cac49347ee5a8a528fbdf31925b9021
LukaszTomaszek <lu.tomaszek@gmail.com>
2020-04-23 17:20:10 +0200

    zad_lab_2304

---
+++ lab_0423.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Proba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.chkButtonEnabled = new System.Windows.Forms.CheckBox();
+            this.txtButtonLabel = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(35, 190);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(237, 33);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "CHANGE ME";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // chkButtonEnabled
+            // 
+            this.chkButtonEnabled.AutoSize = true;
+            this.chkButtonEnabled.Location = new System.Drawing.Point(35, 135);
+            this.chkButtonEnabled.Name = "chkButtonEnabled";
+            this.chkButtonEnabled.Size = new System.Drawing.Size(176, 20);
+            this.chkButtonEnabled.TabIndex = 1;
+            this.chkButtonEnabled.Text = "Włącz / Wyłącz przycisk";
+            this.chkButtonEnabled.UseVisualStyleBackColor = true;
+            this.chkButtonEnabled.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // txtButtonLabel
+            // 
+            this.txtButtonLabel.Location = new System.Drawing.Point(35, 82);
+            this.txtButtonLabel.Name = "txtButtonLabel";
+            this.txtButtonLabel.Size = new System.Drawing.Size(237, 22);
+            this.txtButtonLabel.TabIndex = 2;
+            this.txtButtonLabel.TextChanged += new System.EventHandler(this.txtButtonLabel_TextChanged);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTitle.Location = new System.Drawing.Point(30, 31);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(242, 25);
+            this.lblTitle.TabIndex = 6;
+            this.lblTitle.Text = "Laboratoria KWZP 2020";
+            this.lblTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 372);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtButtonLabel);
+            this.Controls.Add(this.chkButtonEnabled);
+            this.Controls.Add(this.btnClose);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.CheckBox chkButtonEnabled;
+        private System.Windows.Forms.TextBox txtButtonLabel;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
---
+++ lab_0423.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnClose.Enabled = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            btnClose.Enabled = chkButtonEnabled.Checked;
+        }
+
+        private void txtButtonLabel_TextChanged(object sender, EventArgs e)
+        {
+            if(txtButtonLabel.TextLength > 0)
+            {
+                btnClose.Text = txtButtonLabel.Text;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
